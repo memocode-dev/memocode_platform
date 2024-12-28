b053b828a2954e455fd6acada4bb05365b841fb7
audgks5551 <audgsk5551@daum.net>
2024-12-28 14:06:48 +0900

    init

---
+++ src/main/java/dev/memocode/memocode_platform/MemocodePlatformApplication.java
@@ -0,0 +1,13 @@
+package dev.memocode.memocode_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemocodePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemocodePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/memocode/memocode_platform/MemocodePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.memocode.memocode_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemocodePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
